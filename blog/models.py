9c51cbd4bb0d3c810dd386bb58e17cc52ef4a72d
alfianinda <ni.alfianinda@gmail.com>
2020-02-19 11:59:47 +0800

    [Alfianinda] - creating blog models, setting, and migrating to db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# create a blog models, ex:
+    # title
+    # pub_date
+    # body
+    # image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=225)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig', #change
     'jobs.apps.JobsConfig', #change
     'django.contrib.admin',
     'django.contrib.auth',
